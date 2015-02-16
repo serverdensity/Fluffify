45f828b42d6ead174a14f432bb970bf413d4d882
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2015-02-16 19:11:23 +0000

    Rewrite to be able to insert html nodes

--- fluffify/js/content_script.js
+++ fluffify/js/content_script.js
@@ -12,53 +12,62 @@ chrome.storage.sync.get('enabled', function(config){
 
 function walk(node, dict)
 {
+    var excludeElements = ['script', 'style', 'iframe', 'cavas'];
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
 
-	var child, next;
+    var child, next;
 
-	switch ( node.nodeType )
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child )
-			{
-				next = child.nextSibling;
-				walk(child, dict);
-				child = next;
-			}
-			break;
+    switch ( node.nodeType )
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while ( child )
+            {
+                if (child.parentElement.className.indexOf('fluffify-ani') > 0){
+                    continue;
+                } else {
+                    next = child.nextSibling;
+                    walk(child, dict);
+                    child = next;
+                }
+            }
+            break;
 
-		case 3: // Text node
+        case 3: // Text node
             if(node.parentElement.tagName.toLowerCase() != "script") {
                 handleText(node, dict);
             }
-			break;
-	}
+            break;
+    }
 }
 
 function handleText(textNode, dict) {
     var text = textNode.nodeValue;
+    var offset, newNode;
     for(var entry in dict){
         if(entry !== 'enabled'){
-            regex = new RegExp("\\b" + entry, "gi");
-            text = text.replace(regex, dict[entry]);
+            regex = new RegExp("\\b" + entry + "\\b", "gi");
+            // if(regex.test(text)){
+            match = regex.exec(textNode.data);
+            if (match) {
+                offset = match.index;
+            }
+            if(offset !== -1 && offset != undefined && match) {
+                console.log("match!");
+                newNode = textNode.splitText(offset);
+                newNode.data = newNode.data.substr(entry.length);
+
+                var span = document.createElement("span");
+                span.className = "fluffify-ani";
+                span.textContent = dict[entry];
+                textNode.parentNode.insertBefore(span, newNode);
+            }
+
+            // }
         }
     }
-
-    // // Get the corner cases
-    // if (v.match(/cloud/i)) {
-    //     // If we're not talking about weather
-    //     if (v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
-    //         v = v.replace(/(C|c)loud/gi, function(match, p1, offset, string) {
-    //             // c - 1 = b
-    //             b = String.fromCharCode(p1.charCodeAt(0) - 1);
-    //             return b + "utt";
-    //         });
-    //     }
-    // }
-    textNode.nodeValue = text;
 }
 
